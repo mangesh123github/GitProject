118a9e944b3a41f4318d480775411363685bf350
Mangesh <mangeshgujar1321@gmail.com>
2022-09-16 18:36:57 +0530

    Added cod for character array

---
+++ ArrayProject/src/com/mangesh/array/ArrayDemo2.java
@@ -0,0 +1,13 @@
+package com.mangesh.array;
+
+public class ArrayDemo2 {
+
+	public static void main(String[] args) {
+		char ch []= {'f','3','d','#','g'};
+		for (int i = 0; i<ch.length;i++) {
+			System.out.println(ch[i]);
+		}
+
+	}
+
+}
