6921d7e9ab02b948a1bfc305f8407f40c7cbfbd0
Mangesh <mangeshgujar1321@gmail.com>
2022-09-16 13:29:47 +0530

    Added code for string array

---
+++ ArrayProject/src/com/mangesh/array/ArrayDemo1.java
@@ -0,0 +1,13 @@
+package com.mangesh.array;
+
+public class ArrayDemo1 {
+
+	public static void main(String[] args) {
+		String [] str = {"pune","delhi","kolkata","hyderabad","jaipur"};
+		for ( String i :str) {
+			System.out.println(i);
+		}
+
+	}
+
+}
