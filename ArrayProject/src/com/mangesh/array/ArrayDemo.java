4810559deae4c2a65fcd693fa3dcde6a50b62291
Mangesh <mangeshgujar1321@gmail.com>
2022-09-16 12:58:38 +0530

    First commit

---
+++ ArrayProject/src/com/mangesh/array/ArrayDemo.java
@@ -0,0 +1,13 @@
+package com.mangesh.array;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		int [] arr = {15,65,42,6,87,53,23};
+		
+		for(int i = 0;i<arr.length;i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+}
